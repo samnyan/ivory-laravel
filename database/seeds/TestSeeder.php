126f04e2ca9b88989346e0930245b883378aa6f5
samnyan <4137880+samnyan@users.noreply.github.com>
2020-07-03 12:38:58 +0900

    Update database seed value

--- database/seeds/TestSeeder.php
+++ database/seeds/TestSeeder.php
@@ -42,11 +42,11 @@ class TestSeeder extends Seeder
             'type' => 0,
             'sex' => 0,
             'age' => 24,
-            'head_portrait' => 'http://pic136.huitu.com/res/20200110/2350458_20200110022605051080_1.jpg',
+            'head_portrait' => 'https://ivory.r.samnya.cn/api/files/public/avatar.png',
             'clinic_id' => 1,
             'mobile' => '+8613800000000',
             'fix_phone_number' => '',
-            'certificate' => 'http://pic136.huitu.com/res/20200110/2350458_20200110022605051080_1.jpg',
+            'certificate' => 'https://ivory.r.samnya.cn/api/files/public/certificate.jpg',
             'certificate_checked' => 2,
             'wechat' => '00000',
             'intro' => 'To specify a list of valid parameters your API route accepts, use the @urlParam, @bodyParam and @queryParam annotations.',
@@ -80,7 +80,7 @@ class TestSeeder extends Seeder
                 'id' => 1,
                 'name' => '达明口腔门诊部',
                 'city' => '广州',
-                'image' => 'http://pic136.huitu.com/res/20200110/2350458_20200110022605051080_1.jpg',
+                'image' => 'https://ivory.r.samnya.cn/api/files/public/hospital.jpg',
                 'position' => '23.544983,113.595114',
                 'address' => '广州市从化区河东北路5号',
                 'intro' => '暂无介绍'
@@ -96,7 +96,8 @@ class TestSeeder extends Seeder
                 'name' => '某人',
                 'age' => 10,
                 'sex' => 0,
-                'comments' => '无'
+                'comments' => '无',
+                'photo_url' => 'patientPhoto/ogzMlT0vOp53PGgc5KwWmkUZ7mh4ndbFLdH1RS2h.png'
             ]
         ]);
 
